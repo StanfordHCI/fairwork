1ff8d4b260964b31a5893744093975324a70f7ef
Michael Bernstein <msb@cs.stanford.edu>
2018-05-03 10:47:04 -0700

    improving auditing algorithm

--- timerjerk/admin.py
+++ timerjerk/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import HITType, HIT, Worker, Assignment, AssignmentDuration
+from .models import HITType, HIT, Worker, Assignment, AssignmentDuration, AssignmentAudit
 
 admin.site.register(HITType)
 admin.site.register(HIT)
 admin.site.register(Worker)
 admin.site.register(Assignment)
 admin.site.register(AssignmentDuration)
+admin.site.register(AssignmentAudit)
