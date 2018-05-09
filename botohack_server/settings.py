a6362b000cc0ca3bbccd9b87bbdfa603c1d4c0a1
Michael Bernstein <msb@cs.stanford.edu>
2018-05-08 20:41:59 -0700

    fixing allowed hosts

--- botohack_server/settings.py
+++ botohack_server/settings.py
@@ -16,9 +16,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '123456789'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
 
 
 # Application definition
