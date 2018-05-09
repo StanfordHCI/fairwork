e6baf222c86eee92ec41bf2c5d52b1a06fa4500e
Michael Bernstein <msb@cs.stanford.edu>
2018-05-08 22:13:13 -0700

    dj-database-url

--- botohack_server/django_settings.py
+++ botohack_server/django_settings.py
@@ -1,5 +1,6 @@
 from botohack_server.settings import *
 import django_heroku
+import dj_database_url
 
 DEBUG = False
 
@@ -12,5 +13,7 @@ if USE_SANDBOX:
     MTURK_ENDPOINT = 'https://mturk-requester-sandbox.us-east-1.amazonaws.com'
 else:
     MTURK_ENDPOINT = 'https://mturk-requester.us-east-1.amazonaws.com'
+
+DATABASES['default'] =  dj_database_url.config()
 
 django_heroku.settings(locals())
