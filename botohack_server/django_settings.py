6e642087908ae476bb3f9abab305383b1a9af13f
Michael Bernstein <msb@cs.stanford.edu>
2018-05-09 11:13:57 -0700

    debugging django settings loading from os

--- botohack_server/django_settings.py
+++ botohack_server/django_settings.py
@@ -10,6 +10,12 @@ TIME_ZONE = 'America/Los_Angeles'
 
 DATABASES['default'] =  dj_database_url.config()
 
+AWS_ACCESS_KEY_ID = os.environ('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ('AWS_SECRET_ACCESS_KEY')
+SQS_QUEUE = os.environ('SQS_QUEUE')
+SQS_QUEUE_NAME = os.environ('SQS_QUEUE_NAME')
+SQS_REGION_NAME = os.environ('SQS_REGION_NAME')
+
 django_heroku.settings(locals())
 
 print("Django specific settings loaded.")
