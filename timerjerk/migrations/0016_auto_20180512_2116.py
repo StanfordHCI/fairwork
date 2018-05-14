41c2ef6ce39773e81ca74268adfb1f2394cf6bc5
Michael Bernstein <msb@cs.stanford.edu>
2018-05-13 22:24:46 -0700

    trying to debug migration

--- timerjerk/migrations/0016_auto_20180512_2116.py
+++ timerjerk/migrations/0016_auto_20180512_2116.py
@@ -17,7 +17,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='requester',
             name='id',
-            field=models.AutoField(auto_created=True, default=1, primary_key=True, serialize=False, verbose_name='ID'),
+            field=models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID'),
             preserve_default=False,
         ),
     ]
