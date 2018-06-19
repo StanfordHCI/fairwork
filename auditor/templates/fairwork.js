d1f4b1acae016f8f9ab5f19fd0a9098d53ef4558
Michael Bernstein <msb@cs.stanford.edu>
2018-06-19 11:14:53 -0700

    debuggging client

--- auditor/templates/fairwork.js
+++ auditor/templates/fairwork.js
@@ -16,34 +16,6 @@
     }
   }
 
-  // Taken from underscore.js (MIT License)
-  // Similar to ES6's rest param (http://ariya.ofilabs.com/2013/03/es6-and-rest-parameter.html)
-  // This accumulates the arguments passed into an array, after a given index.
-  var restArgs = function(func, startIndex) {
-    startIndex = startIndex == null ? func.length - 1 : +startIndex;
-    return function() {
-      var length = Math.max(arguments.length - startIndex, 0),
-          rest = Array(length),
-          index = 0;
-      for (; index < length; index++) {
-        rest[index] = arguments[index + startIndex];
-      }
-      switch (startIndex) {
-        case 0: return func.call(this, rest);
-        case 1: return func.call(this, arguments[0], rest);
-        case 2: return func.call(this, arguments[0], arguments[1], rest);
-      }
-      var args = Array(startIndex + 1);
-      for (index = 0; index < startIndex; index++) {
-        args[index] = arguments[index];
-      }
-      args[startIndex] = rest;
-      return func.apply(this, args);
-    };
-  };
-
-
-
   // Equivalent of $(document).ready
   document.addEventListener("DOMContentLoaded", function(event) {
 
