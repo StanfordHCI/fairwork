374b3f045ba064b513e99eb3d645e6d08b761af6
Michael Bernstein <msb@cs.stanford.edu>
2018-06-19 14:51:37 -0700

    turning off notifications to test bug

--- auditor/views.py
+++ auditor/views.py
@@ -59,14 +59,14 @@ def create_hit(request):
             requester = r
         )
 
-    client.update_notification_settings(
-        HITTypeId=ht.id,
-        Notification={
-            'Destination': settings.SQS_QUEUE,
-            'Transport': 'SQS',
-            'Version': '2014-08-15',
-            'EventTypes': [ 'AssignmentApproved' ]
-        }
+    # client.update_notification_settings(
+    #     HITTypeId=ht.id,
+    #     Notification={
+    #         'Destination': settings.SQS_QUEUE,
+    #         'Transport': 'SQS',
+    #         'Version': '2014-08-15',
+    #         'EventTypes': [ 'AssignmentApproved' ]
+    #     }
     )
 
     h, h_created = HIT.objects.get_or_create(
