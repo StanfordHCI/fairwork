187a86aa8e76ee3f8c389c0b4a99670ee01c691d
Michael Bernstein <msb@cs.stanford.edu>
2018-08-03 14:57:47 -0700

    fixed auditing; added email

--- auditor/management/commands/auditpayments.py
+++ auditor/management/commands/auditpayments.py
@@ -1,6 +1,7 @@
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
-from django.db.models import Avg, Sum
+from django.db.models import Avg, Sum, F
+from django.core.mail import send_mail
 
 from statistics import median
 from decimal import Decimal
@@ -79,67 +80,113 @@ class Command(BaseCommand):
     def __pay_audited_hits(self):
         for is_sandbox in [True, False]:
             self.stdout.write(self.style.WARNING('Sandbox mode: %s' % is_sandbox))
+            audits = AssignmentAudit.objects.filter(status = AssignmentAudit.UNPAID)
             if is_sandbox:
-                audits = AssignmentAudit.objects.filter(assignment__hit__hit_type__host__contains = 'sandbox')
+                audits = audits.filter(assignment__hit__hit_type__host__contains = 'sandbox')
             else:
-                audits = AssignmentAudit.objects.exclude(assignment__hit__hit_type__host__contains = 'sandbox')
+                audits = audits.exclude(assignment__hit__hit_type__host__contains = 'sandbox')
 
-            assignments_to_bonus = audits.filter(status = AssignmentAudit.UNPAID).order_by('assignment__hit__hit_type__requester', 'assignment__worker', 'assignment__hit__hit_type')
+            requesters = Requester.objects.filter(hittype__hit__assignment__assignmentaudit__in = audits).distinct()
+            for requester in requesters:
+                self.stdout.write(self.style.WARNING('Requester: %s %s' % (requester.aws_account, requester.email)))
 
-            per_requester = itertools.groupby(assignments_to_bonus, key=lambda x: x.assignment.hit.hit_type.requester)
-            for requester, requester_to_bonus in per_requester:
-                self.stdout.write(self.style.WARNING('Requester: %s' % requester.aws_account))
+                requester_to_bonus = audits.filter(assignment__hit__hit_type__requester = requester).order_by('assignment__hit', 'assignment__hit__hit_type', 'assignment__worker')
 
-                per_worker = itertools.groupby(list(requester_to_bonus), key=lambda x: x.assignment.worker)
-                for worker, tasks in per_worker:
-                    # How much do we owe them?
-                    unpaid_tasks = list(tasks) # we're going to reuse this iterator later, so listify it
-                    self.stdout.write(self.style.WARNING('Worker: %s' % worker.id))
-                    total_unpaid = Decimal(0)
-                    for unpaid_task in unpaid_tasks:
-                        self.stdout.write(str(unpaid_task))
-                        total_unpaid += unpaid_task.get_underpayment()
-                    self.stdout.write(self.style.WARNING('Total bonus for %s: $%.2f\n---------' % (worker.id, total_unpaid)))
+                self.__notify_requester(requester, requester_to_bonus)
 
-                    # Construct the message to the worker
-                    message = """This requester is using the Fair Work script to ensure pay rates reach a minimum wage of $%.2f/hr. The goal of fair pay is outlined in the Turker-authored We Are Dynamo guidelines: http://guidelines.wearedynamo.org/. Fair Work does this by asking for completion times and then auto-bonusing workers to meet the desired hourly wage. Based on worker time reports, your tasks have been underpaid. We are bonusing you to bring you back up to $%.2f/hr.
+                workers = Worker.objects.filter(assignment__assignmentaudit__in = requester_to_bonus).distinct()
+                for worker in workers:
+                    assignments_to_bonus = requester_to_bonus.filter(assignment__worker = worker).distinct()
+                    self.__bonus_worker(worker, assignments_to_bonus, requester, is_sandbox)
 
-The tasks being reimbursed:
+    def __bonus_worker(self, worker, assignments_to_bonus, requester, is_sandbox):
+        # How much do we owe them?
+        self.stdout.write(self.style.WARNING('Worker: %s' % worker.id))
+        total_unpaid = self.__get_underpayment(assignments_to_bonus)
+        self.stdout.write(self.style.WARNING('Total bonus for %s: $%.2f\n---------' % (worker.id, total_unpaid)))
 
-""" % (settings.MINIMUM_WAGE_PER_HOUR, settings.MINIMUM_WAGE_PER_HOUR)
+        # Send the bonus
+        # Construct the message to the worker
+        message = self.__audit_list_message(assignments_to_bonus, True, False)
+        assignment_to_bonus = assignments_to_bonus[0] # Arbitrarily attach it to the first one
+        token = '%s: %.2f' % (assignment_to_bonus.assignment.id, total_unpaid) # sending the same token prevents AMT from sending the same bonus twice
 
-                    unpaid_by_hit_type = itertools.groupby(unpaid_tasks, key=lambda x: x.assignment.hit.hit_type)
-                    for hit_type, hit_type_tasks in unpaid_by_hit_type:
-                        tasks = list(hit_type_tasks) # we will reuse, so we need to listify
-                        s = "HIT Type %s originally paid $%.2f per task. Median estimated time across workers was %s, for an estimated rate of $%.2f/hr. Bonus $%.2f for each of %d HITs to bring the payment to $%.2f each. Total: $%.2f bonus\n" % (hit_type.id, hit_type.payment, tasks[0].estimated_time, tasks[0].estimated_rate, tasks[0].get_underpayment(), len(tasks), (hit_type.payment + tasks[0].get_underpayment()),  sum([x.get_underpayment() for x in tasks]))
-                        assignment_ids = [x.assignment.id for x in tasks]
-                        s += "\tAssignments: %s\n\n" % (", ".join(assignment_ids))
-                        message += s
+        mturk_clients = get_mturk_connection(requester, self.mturk)
+        if is_sandbox:
+            mturk_client = mturk_clients['sandbox']
+        else:
+            mturk_client = mturk_clients['production']
 
-                    # Send the bonus
-                    assignment_to_bonus = unpaid_tasks[0] # Arbitrarily attach it to the first one
-                    token = '%s: %.2f' % (assignment_to_bonus.assignment.id, total_unpaid) # sending the same token prevents AMT from sending the same bonus twice
+        try:
+            response = mturk_client.send_bonus(WorkerId = worker.id, BonusAmount = '%.2f' % (total_unpaid), AssignmentId = assignment_to_bonus.assignment.id, Reason = message, UniqueRequestToken = token)
+
+            # Once the bonus is sent, mark the audits as paid
+            for unpaid_task in assignments_to_bonus:
+                unpaid_task.status = AssignmentAudit.PAID
+                unpaid_task.save()
+        except mturk_client.exceptions.RequestError as e:
+            if e.response['Error']['Message'].startswith("This Requester has insufficient funds in their account to complete this transaction."):
+                self.stderr.write(self.style.ERROR("Requester does not have enough funds. Notifying worker."))
+                self.__notify_insufficient_funds(requester, is_sandbox, worker, total_unpaid, assignment_to_bonus)
+            else:
+                self.stderr.write(self.style.ERROR(e))
 
 
-                    mturk_clients = get_mturk_connection(requester, self.mturk)
-                    if is_sandbox:
-                        mturk_client = mturk_clients['sandbox']
-                    else:
-                        mturk_client = mturk_clients['production']
+    def __get_underpayment(self, assignments_to_bonus):
+        total_unpaid = Decimal(0)
+        for unpaid_task in assignments_to_bonus:
+            total_unpaid += unpaid_task.get_underpayment()
+        return total_unpaid
 
-                    try:
-                        response = mturk_client.send_bonus(WorkerId = worker.id, BonusAmount = '%.2f' % (total_unpaid), AssignmentId = assignment_to_bonus.assignment.id, Reason = message, UniqueRequestToken = token)
+    def __audit_list_message(self, assignments_to_bonus, is_worker, is_html):
+        total_unpaid = self.__get_underpayment(assignments_to_bonus)
+        message = ""
 
-                        # Once the bonus is sent, mark the audits as paid
-                        for unpaid_task in unpaid_tasks:
-                            unpaid_task.status = AssignmentAudit.PAID
-                            unpaid_task.save()
-                    except mturk_client.exceptions.RequestError as e:
-                        if e.response['Error']['Message'].startswith("This Requester has insufficient funds in their account to complete this transaction."):
-                            self.stderr.write(self.style.ERROR("Requester does not have enough funds. Notifying worker."))
-                            self.__notify_insufficient_funds(requester, is_sandbox, worker, total_unpaid, assignment_to_bonus)
-                        else:
-                            self.stderr.write(self.style.ERROR(e))
+        message += "<p>" if is_html else ""
+
+        if is_worker:
+            message = "This requester is "
+        else:
+            message = "You are "
+        message += "using the Fair Work script to ensure pay rates reach a minimum wage of $%.2f/hr. The goal of fair pay is outlined in the Turker-authored We Are Dynamo guidelines: http://guidelines.wearedynamo.org/. Fair Work does this by asking for completion times and then auto-bonusing workers to meet the desired hourly wage. Based on worker time reports, some of your tasks have been underpaid. The Fair Work system is bonusing workers to bring payment back up to $%.2f/hr." % (settings.MINIMUM_WAGE_PER_HOUR, settings.MINIMUM_WAGE_PER_HOUR)
+        message += "</p>" if is_html else "\n\n"
+
+        message += "<p>" if is_html else ""
+        message += "The total amount paid is $%.2f. The tasks being reimbursed:" % total_unpaid
+        message += "</p><ul>" if is_html else "\n\n"
+
+        hit_types = HITType.objects.filter(hit__assignment__assignmentaudit__in = assignments_to_bonus).distinct()
+        for hit_type in hit_types:
+            hittype_assignments = assignments_to_bonus.filter(assignment__hit__hit_type = hit_type)
+            s = "<li>" if is_html else ""
+            s += "HIT Type %s originally paid $%.2f per task. Median estimated time across workers was %s, for an estimated rate of $%.2f/hr. Bonus $%.2f for each of %d assignments to bring the payment to $%.2f each. Total: $%.2f bonus." % (hit_type.id, hit_type.payment, hittype_assignments[0].estimated_time, hittype_assignments[0].estimated_rate, hittype_assignments[0].get_underpayment(), len(hittype_assignments), (hit_type.payment + hittype_assignments[0].get_underpayment()),  sum([x.get_underpayment() for x in hittype_assignments]))
+            s += "<ul>" if is_html else "\n"
+
+            hits = HIT.objects.filter(assignment__assignmentaudit__in = hittype_assignments).distinct()
+            for hit in hits:
+                s += "<li>" if is_html else "\t"
+                s += "HIT %s: " % hit.id
+                hit_assignments = hittype_assignments.filter(assignment__hit = hit)
+                assignment_ids = [x.assignment.id for x in hit_assignments]
+                if len(assignment_ids) == 1:
+                    s += "assignment %s" % ", ".join(assignment_ids)
+                else:
+                    s += "assignments %s" % ", ".join(assignment_ids)
+                s += "</li>" if is_html else "\n"
+            s += "</li>" if is_html else "\n\n"
+            message += s
+        return message
+
+    def __notify_requester(self, requester, assignments_to_bonus):
+        email = requester.email
+        self.stdout.write(email)
+        plain_message = self.__audit_list_message(assignments_to_bonus, False, False)
+        html_message = self.__audit_list_message(assignments_to_bonus, False, True)
+        self.stdout.write(plain_message)
+
+        subject = "Fair Work: Mechanical Turk bonuses sent for $%.2f" % self.__get_underpayment(assignments_to_bonus)
+        send_mail(subject, plain_message, 'fairwork@cs.stanford.edu', [email], fail_silently=False, html_message=html_message)
+
 
     def __notify_insufficient_funds(self, requester, is_sandbox, worker, total_unpaid, assignment_to_bonus):
         """
