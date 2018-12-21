00548e86cd3af2c828e040e8f4e5779a8612c8b4
Michael Bernstein <msb@cs.stanford.edu>
2018-12-20 20:15:14 -0800

    paying in payaudits, not auditpayments

--- auditor/management/commands/auditpayments.py
+++ auditor/management/commands/auditpayments.py
@@ -2,19 +2,19 @@ from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
 from django.db.models import Avg, Sum, F
 from django.core.mail import send_mail
+from django.utils import timezone
 
 from statistics import median
 import decimal
 from decimal import Decimal
 import math
 import itertools
-from django.utils import timezone
+from datetime import timedelta
 
 import boto3
 
 from auditor.models import HITType, HIT, Worker, Assignment, AssignmentDuration, AssignmentAudit, Requester
 from auditor.management.commands.pullnotifications import get_mturk_connection
-from auditor.management.commands.payaudits import REQUESTER_GRACE_PERIOD
 
 """
 Performs the payment audit on the task. Pseudocode:
@@ -27,8 +27,10 @@ Performs the payment audit on the task. Pseudocode:
 - Bonus worker on first assignment in the HITGroup (to avoid being spammy) and keep a record
 """
 
+REQUESTER_GRACE_PERIOD = timedelta(hours = 12)
+
 class Command(BaseCommand):
-    help = 'Calculate effective rate for tasks and bonus any underpayment'
+    help = 'Calculate effective rate for tasks and audit any underpayment'
 
     mturk = dict() # maintains the Boto client connections
 
@@ -81,16 +83,6 @@ class Command(BaseCommand):
 
             return auditable
 
-    def __get_underpayment(self, assignments_to_bonus):
-        total_unpaid = Decimal('0.00')
-        for unpaid_task in assignments_to_bonus:
-            underpayment = unpaid_task.get_underpayment()
-            if underpayment is not None:
-                total_unpaid += underpayment
-        # don't shortchange workers --- round up to the nearest cent
-        total_unpaid = math.ceil(total_unpaid * Decimal('100.0')) / Decimal('100.0')
-        return total_unpaid
-
     def __notify_requesters(self):
         audits = AssignmentAudit.objects.filter(message_sent = None)
         print("Audits?")
@@ -103,11 +95,11 @@ class Command(BaseCommand):
     def __notify_requester(self, requester, requester_audit):
         email = requester.email
         self.stdout.write(email)
-        plain_message = self.__audit_list_message(requester_audit, False, False)
-        html_message = self.__audit_list_message(requester_audit, False, True)
+        plain_message = audit_list_message(requester_audit, False, False)
+        html_message = audit_list_message(requester_audit, False, True)
         self.stdout.write(plain_message)
 
-        subject = "Fair Work: Mechanical Turk bonuses pending for $%.2f" % self.__get_underpayment(requester_audit)
+        subject = "Fair Work: Mechanical Turk bonuses pending for $%.2f" % get_underpayment(requester_audit)
         send_mail(subject, plain_message, admin_email_address(), [email], fail_silently=False, html_message=html_message)
 
         sent_time = timezone.now()
@@ -115,61 +107,76 @@ class Command(BaseCommand):
             audit.message_sent = sent_time
             audit.save()
 
-    def __audit_list_message(self, assignments_to_bonus, is_worker, is_html):
-        total_unpaid = self.__get_underpayment(assignments_to_bonus)
-        message = ""
 
+# Expose these methods publicly
+
+def audit_list_message(assignments_to_bonus, is_worker, is_html):
+    total_unpaid = get_underpayment(assignments_to_bonus)
+    message = ""
+
+    message += "<p>" if is_html else ""
+
+    if is_worker:
+        message = "This requester is "
+    else:
+        message = "You are "
+    message += "using the Fair Work script to ensure pay rates reach a minimum wage of $%.2f/hr. The goal of fair pay is outlined in the Turker-authored We Are Dynamo guidelines: http://guidelines.wearedynamo.org/. Fair Work does this by asking for completion times and then auto-bonusing workers to meet the desired hourly wage of $%.2f/hr." % (settings.MINIMUM_WAGE_PER_HOUR, settings.MINIMUM_WAGE_PER_HOUR)
+    message += "</p>" if is_html else "\n\n"
+
+    if not is_worker:
         message += "<p>" if is_html else ""
+        message += "Bonuses will be sent in %d hours: %s. You can review the pending bonuses below and freeze bonuses if something looks unusual. Please remember to trust the workers' estimates, and only freeze bonuses if absolutely needed." % (REQUESTER_GRACE_PERIOD.total_seconds() / (60*60), timezone.localtime(timezone.now() + REQUESTER_GRACE_PERIOD).strftime("%B %d at %-I:%M%p %Z"))
 
-        if is_worker:
-            message = "This requester is "
+    message += "<p>" if is_html else ""
+    message += "The total amount paid is $%.2f. The tasks being reimbursed:" % total_unpaid
+    message += "</p><ul>" if is_html else "\n\n"
+
+    hit_types = HITType.objects.filter(hit__assignment__assignmentaudit__in = assignments_to_bonus).distinct()
+    for hit_type in hit_types:
+        hittype_assignments = assignments_to_bonus.filter(assignment__hit__hit_type = hit_type)
+        s = "<li>" if is_html else ""
+
+        underpayment = hittype_assignments[0].get_underpayment()
+        time_nomicroseconds = str(hittype_assignments[0].estimated_time).split(".")[0]
+        if underpayment is None:
+            summary = "HIT Type {hittype:s} originally paid ${payment:.2f} per task. No workers reported time elapsed for this HIT, so effective rate cannot be estimated. No bonuses will be sent.".format(hittype = hit_type.id, payment = hit_type.payment)
+        elif underpayment <= Decimal('0.00'):
+            summary = "HIT Type {hittype:s} originally paid ${payment:.2f} per task. Median estimated time across workers was {estimated:s}, for an estimated rate of ${paymentrate:.2f}/hr. No bonus necessary.".format(hittype = hit_type.id, payment = hit_type.payment, estimated = time_nomicroseconds, paymentrate = hittype_assignments[0].estimated_rate)
         else:
-            message = "You are "
-        message += "using the Fair Work script to ensure pay rates reach a minimum wage of $%.2f/hr. The goal of fair pay is outlined in the Turker-authored We Are Dynamo guidelines: http://guidelines.wearedynamo.org/. Fair Work does this by asking for completion times and then auto-bonusing workers to meet the desired hourly wage of $%.2f/hr." % (settings.MINIMUM_WAGE_PER_HOUR, settings.MINIMUM_WAGE_PER_HOUR)
-        message += "</p>" if is_html else "\n\n"
+            paymentrevised = hit_type.payment + hittype_assignments[0].get_underpayment()
+            bonus = underpayment.quantize(Decimal('1.000')).normalize() if underpayment >= Decimal(0.01) else underpayment.quantize(Decimal('1.000'))
+            paymentrevised = paymentrevised.quantize(Decimal('1.000')).normalize() if paymentrevised >= Decimal(0.01) else paymentrevised.quantize(Decimal('1.000'))
 
-        if not is_worker:
-            message += "<p>" if is_html else ""
-            message += "Bonuses will be sent in %d hours: %s. You can review the pending bonuses below and freeze bonuses if something looks unusual. Please remember to trust the workers' estimates, and only freeze bonuses if absolutely needed." % (REQUESTER_GRACE_PERIOD.total_seconds() / (60*60), timezone.localtime(timezone.now() + REQUESTER_GRACE_PERIOD).strftime("%B %d at %-I:%M%p %Z"))
+            summary = "HIT Type {hittype:s} originally paid ${payment:.2f} per task. Median estimated time across workers was {estimated:s}, for an estimated rate of ${paymentrate:.2f}/hr. Bonus ${bonus:f} for each of {num_assignments:d} assignments to bring the payment to a suggested ${paymentrevised:f} each. Total: ${totalbonus:.2f} bonus.".format(hittype = hit_type.id, payment = hit_type.payment, estimated = time_nomicroseconds, paymentrate = hittype_assignments[0].estimated_rate, bonus = bonus, num_assignments = len(hittype_assignments), paymentrevised = paymentrevised, totalbonus = get_underpayment(hittype_assignments))
+            if not is_worker:
+                summary += " [FREEZE LINK]"
+        s += summary
+        s += "<ul>" if is_html else "\n"
 
-        message += "<p>" if is_html else ""
-        message += "The total amount paid is $%.2f. The tasks being reimbursed:" % total_unpaid
-        message += "</p><ul>" if is_html else "\n\n"
-
-        hit_types = HITType.objects.filter(hit__assignment__assignmentaudit__in = assignments_to_bonus).distinct()
-        for hit_type in hit_types:
-            hittype_assignments = assignments_to_bonus.filter(assignment__hit__hit_type = hit_type)
-            s = "<li>" if is_html else ""
-
-            underpayment = hittype_assignments[0].get_underpayment()
-            time_nomicroseconds = str(hittype_assignments[0].estimated_time).split(".")[0]
-            if underpayment is None:
-                summary = "HIT Type {hittype:s} originally paid ${payment:.2f} per task. No workers reported time elapsed for this HIT, so effective rate cannot be estimated. No bonuses will be sent.".format(hittype = hit_type.id, payment = hit_type.payment)
-            elif underpayment <= Decimal('0.00'):
-                summary = "HIT Type {hittype:s} originally paid ${payment:.2f} per task. Median estimated time across workers was {estimated:s}, for an estimated rate of ${paymentrate:.2f}/hr. No bonus necessary.".format(hittype = hit_type.id, payment = hit_type.payment, estimated = time_nomicroseconds, paymentrate = hittype_assignments[0].estimated_rate)
+        hits = HIT.objects.filter(assignment__assignmentaudit__in = hittype_assignments).distinct()
+        for hit in hits:
+            s += "<li>" if is_html else "\t"
+            s += "HIT %s: " % hit.id
+            hit_assignments = hittype_assignments.filter(assignment__hit = hit)
+            assignment_ids = [x.assignment.id for x in hit_assignments]
+            if len(assignment_ids) == 1:
+                s += "assignment %s" % ", ".join(assignment_ids)
             else:
-                paymentrevised = hit_type.payment + hittype_assignments[0].get_underpayment()
-                bonus = underpayment.quantize(Decimal('1.000')).normalize() if underpayment >= Decimal(0.01) else underpayment.quantize(Decimal('1.000'))
-                paymentrevised = paymentrevised.quantize(Decimal('1.000')).normalize() if paymentrevised >= Decimal(0.01) else paymentrevised.quantize(Decimal('1.000'))
+                s += "assignments %s" % ", ".join(assignment_ids)
+            s += "</li>" if is_html else "\n"
+        s += "</li>" if is_html else "\n\n"
+        message += s
+    return message
 
-                summary = "HIT Type {hittype:s} originally paid ${payment:.2f} per task. Median estimated time across workers was {estimated:s}, for an estimated rate of ${paymentrate:.2f}/hr. Bonus ${bonus:f} for each of {num_assignments:d} assignments to bring the payment to a suggested ${paymentrevised:f} each. Total: ${totalbonus:.2f} bonus. [FREEZE LINK]".format(hittype = hit_type.id, payment = hit_type.payment, estimated = time_nomicroseconds, paymentrate = hittype_assignments[0].estimated_rate, bonus = bonus, num_assignments = len(hittype_assignments), paymentrevised = paymentrevised, totalbonus = self.__get_underpayment(hittype_assignments))
-            s += summary
-            s += "<ul>" if is_html else "\n"
-
-            hits = HIT.objects.filter(assignment__assignmentaudit__in = hittype_assignments).distinct()
-            for hit in hits:
-                s += "<li>" if is_html else "\t"
-                s += "HIT %s: " % hit.id
-                hit_assignments = hittype_assignments.filter(assignment__hit = hit)
-                assignment_ids = [x.assignment.id for x in hit_assignments]
-                if len(assignment_ids) == 1:
-                    s += "assignment %s" % ", ".join(assignment_ids)
-                else:
-                    s += "assignments %s" % ", ".join(assignment_ids)
-                s += "</li>" if is_html else "\n"
-            s += "</li>" if is_html else "\n\n"
-            message += s
-        return message
+def get_underpayment(assignments_to_bonus):
+    total_unpaid = Decimal('0.00')
+    for unpaid_task in assignments_to_bonus:
+        underpayment = unpaid_task.get_underpayment()
+        if underpayment is not None:
+            total_unpaid += underpayment
+    # don't shortchange workers --- round up to the nearest cent
+    total_unpaid = math.ceil(total_unpaid * Decimal('100.0')) / Decimal('100.0')
+    return total_unpaid
 
 def admin_email_address():
     return "%s <%s>" % (settings.ADMINS[0][0], settings.ADMINS[0][1])
