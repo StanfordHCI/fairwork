8533166d48dfd347b7337dc81056e86c015c411c
Grant Hugh <granthugh@DN51vdms.SUNet>
2019-05-06 22:41:25 -0700

    Debugging

--- auditor/management/commands/payaudits.py
+++ auditor/management/commands/payaudits.py
@@ -37,19 +37,14 @@ class Command(BaseCommand):
             self.stdout.write(self.style.WARNING('Sandbox mode: %s' % is_sandbox))
             audits = AssignmentAudit.objects.filter(closed = False).filter(needsPayment = True).filter(message_sent__lte = grace_period_limit)
 
-            # change audits that have status no payment needed to some new status called processed
-            # nopaymentneeded = AssignmentAudit.objects.filter(status=AssignmentAudit.NO_PAYMENT_NEEDED)
-            # for assignmentaudit in nopaymentneeded:
-            #     assignmentaudit.status = AssignmentAudit.PROCESSED
-            #     assignmentaudit.full_clean()
-            #     assignmentaudit.save()
-
             if is_sandbox:
                 audits = audits.filter(assignment__hit__hit_type__host__contains = 'sandbox')
             else:
                 audits = audits.exclude(assignment__hit__hit_type__host__contains = 'sandbox')
 
             requesters = Requester.objects.filter(hittype__hit__assignment__assignmentaudit__in = audits).distinct()
+            print("requesters")
+            print(requesters)
             for requester in requesters:
                 self.stdout.write(self.style.WARNING('Requester: %s %s' % (requester.aws_account, requester.email)))
 
@@ -57,6 +52,8 @@ class Command(BaseCommand):
 
                 workers = Worker.objects.filter(assignment__assignmentaudit__in = requester_to_bonus).distinct()
                 for worker in workers:
+                    print("worker")
+                    print(worker)
                     assignments_to_bonus = requester_to_bonus.filter(assignment__worker = worker).distinct()
                     self.__bonus_worker(worker, assignments_to_bonus, requester, is_sandbox)
 
@@ -76,6 +73,8 @@ class Command(BaseCommand):
         # Send the bonus
         # Construct the message to the worker
         message = auditpayments.audit_list_message(assignments_to_bonus, requester, True, False, is_sandbox)
+        print("message")
+        print(message)
         # Bonus worker on first assignment in the HITGroup (to avoid being spammy) and keep a record
         assignment_to_bonus = assignments_to_bonus[0] # Arbitrarily attach it to the first one
         token = '%s: %.2f' % (assignment_to_bonus.assignment.id, total_unpaid) # sending the same token prevents AMT from sending the same bonus twice
@@ -103,7 +102,7 @@ class Command(BaseCommand):
                 self.stderr.write(self.style.ERROR("Identical bonus has already been paid on this task. Skipping."))
                 # They already paid it, mark it as done
                 for unpaid_task in assignments_to_bonus:
-                    unpaid_task.closed= True
+                    unpaid_task.closed = True
                     unpaid_task.needsPayment = False
                     unpaid_task.save()
             else:
